e56aa82e5865bdcc6c9b4238fadc1e604eea16fc
resethread <resethread@gmail.com>
2021-02-15 18:20:02 +0100

    add error if setup already done

--- routes/web.js
+++ routes/web.js
@@ -1,22 +1,30 @@
 const express = require('express')
 const router = express.Router()
 
+const Meta = require('../models/Meta')
+
 require('dotenv').config()
 
 const GatewayController = require('../controllers/GatewayController')
 
 router.get('/', GatewayController.index)
 
-router.get('/setup', (req, res) => {
-	res.render('setup.html')
+router.get('/setup',  async (request, response) => {
+
+	const setup_done = await Meta.findOne({ key: 'setup_done' })
+	if ( setup_done ) {
+		return response.render('setup_error.html')
+	}
+
+	response.render('setup.html')
 })
 
-router.get('/login', (req, res) => {
-	res.render('login.html')
+router.get('/login', (request, response) => {
+	response.render('login.html')
 })
 
-router.get('/dashboard', (req, res) => {
-	res.render('dashboard.html')
+router.get('/dashboard', (request, response) => {
+	response.render('dashboard.html')
 })
 
 module.exports = router
