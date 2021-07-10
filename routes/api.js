e4bd3d8a5700b8910902774abc206047d199b354
netwarp <resethread@gmail.com>
2021-07-10 15:01:25 +0200

    verifyToken middleware

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 
 const jwt = require('jsonwebtoken')
 
+const verifyToken = require('../middleware/veryToken')
+
 const RPC_Controller = require('../controllers/API/RPC_Controller')
 
 const InvoicesController = require('../controllers/API/InvoicesController')
@@ -12,22 +14,34 @@ router.post('/login', (request, response) => {
 
     const user = {
         id: 1,
-        username: 'brad',
-        email: 'brad@gmail.com'
+        username: 'toto',
+        email: 'toto@toto.com'
     }
 
     const privateKey = fs.readFileSync('storage/keys/private.key');
 
-    jwt.sign({user}, privateKey, { expiresIn: '180s' }, (err, token) => {
+    jwt.sign({ user }, privateKey, { expiresIn: '180s' }, (err, token) => {
         response.json({
             token
         })
     })
 })
 
+router.post('/test1', verifyToken, (request, response) => {
+    jwt.verify(request.token, fs.readFileSync('storage/keys/private.key'), (error, auth_data) => {
+        if (error) {
+            response.sendStatus(403)
+        } else {
+            response.json({
+                auth_data
+            })
+        }
+    })
+})
+
 router.post('/rpc', RPC_Controller.fetch)
 
-router.get('/invoices', InvoicesController.index)
+router.get('/invoices', verifyToken, InvoicesController.index)
 router.post('/invoices', InvoicesController.store)
 router.get('/invoices/:id', InvoicesController.show)
 router.put('/invoices/:id', InvoicesController.update)
